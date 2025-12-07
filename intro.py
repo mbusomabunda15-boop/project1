7710f79608799a7ab63929116b17c52d1f486a9f
mbusomabunda15-boop <mbusomabunda15@gmail.com>
2025-12-08 00:34:41 +0200

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,7 @@
+ name=input("my name is")
+surname=input("my surname is")
+age=int(input("enter age"))
+weight=
+
+print(f"hello{name},you are {age}years old i weigh {weight}")
+ 
---
+++ votes.py
@@ -0,0 +1,11 @@
+def votercheck(name:)
+    name=input("enter name:")
+age=int(input("enter your age:"))
+if age >= 18:
+    print(f" hello {name:}) you are not allowed to vote")
+else:
+    print (f"sorry{name:} you are not allowed to vote yet.")
+    for i in range(3):
+        print(name:)
+votercheck        
+       
